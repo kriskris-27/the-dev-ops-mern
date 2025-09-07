1dcfbaf818079ea5cec8e05f719b33271592cae7
Kris <krishnakumars0101@gmail.com>
2025-09-08 01:15:07 +0530

    server started succesfully

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,4 +4,9 @@ const app = express();
 
 app.use(express.json())
 
+app.get("/",(req,res)=>{
+    // res.json({message:"Hello world"})
+    res.send("hi all!!!")
+})
+
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,8 @@
- 
+import app from "./app"
+import {config} from "./config/env"
+
+
+app.listen(config.PORT,()=>{
+    console.log(`Server is running on http://localhost:${config.PORT}`);
+    
+})
