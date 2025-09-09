d48f3611765d7397a22c090d348d6afb0c5e9aaf
Kris <krishnakumars0101@gmail.com>
2025-09-09 15:50:09 +0530

    add todo route executed

--- backend/src/controllers/todoContollers.ts
+++ backend/src/controllers/todoContollers.ts
@@ -6,3 +6,12 @@ export const getAll =async (req:Request , res:Response) =>{
     const todos = await Todo.find({sessionId});
     res.json(todos)
 }
+
+export const addTodo = async(req:Request,res:Response) =>{
+    const {title} = req.body;
+    const {sessionId} = req.cookies;
+
+    const todo = Todo.create({title,iscompletedcompleted:false,sessionId})
+    res.json(todo)
+}
+
